c0212e1edf310885e0fb995d05528ee99ec23774
Krish14hna <92813492+Krish14hna@users.noreply.github.com>
2022-05-28 23:44:46 +0530

    Add files via upload

---
+++ multiply matrix by numpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+p = [[1, 4], [3, 3]]
+q = [[6, 5], [9, 7]]
+print("Matrix p :")
+print(p)
+print("Matrix q :")
+print(q)
+
+result = np.dot(p, q)
+
+print("The matrix multiplication is :")
+print(result)
